fcce53ca7a5a32617e73acad67566e462473847d
JacekK <info@jacek-k.net>
2015-01-04 06:09:32 +0100

    cleaned dependencies;

--- Gruntfile.js
+++ Gruntfile.js
@@ -376,20 +376,20 @@ module.exports = function (grunt) {
 
         // Generates a custom Modernizr build that includes only the tests you
         // reference in your app
-        modernizr: {
-            dist: {
-                devFile: 'bower_components/modernizr/modernizr.js',
-                outputFile: '<%= config.dist %>/scripts/vendor/modernizr.js',
-                files: {
-                    src: [
-                        '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}*.css',
-                        '!<%= config.dist %>/scripts/vendor/*'
-                    ]
-                },
-                uglify: true
-            }
-        },
+        // modernizr: {
+        //     dist: {
+        //         devFile: 'bower_components/modernizr/modernizr.js',
+        //         outputFile: '<%= config.dist %>/scripts/vendor/modernizr.js',
+        //         files: {
+        //             src: [
+        //                 '<%= config.dist %>/scripts/{,*/}*.js',
+        //                 '<%= config.dist %>/styles/{,*/}*.css',
+        //                 '!<%= config.dist %>/scripts/vendor/*'
+        //             ]
+        //         },
+        //         uglify: true
+        //     }
+        // },
 
         // Run some tasks in parallel to speed up build process
         concurrent: {
@@ -460,7 +460,7 @@ module.exports = function (grunt) {
         'cssmin',
         'uglify',
         'copy:dist',
-        'modernizr',
+        // 'modernizr',
         'rev',
         'usemin',
         'htmlmin'
